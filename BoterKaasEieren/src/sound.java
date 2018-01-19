9b1391c276d955d041fd37b624991234a155c2bc
Taco Boerkamp <tboerkamp@hotmail.com>
2018-01-19 20:18:34 +0100

    #17 met geluid voor nieuwe zet.

---
+++ BoterKaasEieren/src/sound.java
@@ -0,0 +1,17 @@
+//import sun.audio.*;
+//import java.io.*;
+
+//public class sound {
+
+//	public static void main(String[] args)
+//	throws Exception
+//	{
+//		String wav_file = "C:\\Users\\Taco\\eclipse-workspace\\BoterKaasEieren\\0564.wav";
+	
+//		InputStream in = new FileInputStream(wav_file);
+		
+//		AudioStream audio = new AudioStream(in);
+//		AudioPlayer.player.start(audio);
+//	}
+//}
+
