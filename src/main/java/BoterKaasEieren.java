d8a6beb2472cd882f6847c906b9f975ba4ca5870
Martine Verburgh <martineverburgh@gmail.com>
2018-01-24 21:34:35 +0100

    Try-catchblok uitgecommentarieerd.

--- src/main/java/BoterKaasEieren.java
+++ src/main/java/BoterKaasEieren.java
@@ -158,12 +158,12 @@ public class BoterKaasEieren {
 		int col = Integer.parseInt(coords[0]);
 		int row = Integer.parseInt(coords[1]);
 
-        try {
+ /*       try {
             playsNewSetSound();
         } catch (IOException e) {
             System.out.println("Something went wrong playing the tunes " + e);
         }
-
+*/
 
         //Let op hoe we opnieuw door een twee-dimensionale array lopen
 		if ((row >= 0) && (row < 3)) {
